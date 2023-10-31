e705beae265a3a7e6fb2070aeb6b73bbc99000d0
1E10 Laptop <sample@bths.edu>
2023-10-31 10:57:27 -0400

    w
    Co-authored-by: zubing2 <zubing2@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //Wei Work
 let score = 0;
 let revealedAmount = 0;
+let activeTile;
 let emojis = [
   "🎃",
   "🍁",
@@ -27,6 +28,30 @@ function createBoard() {
     let tile = document.createElement("div");
     tile.className = "tile";
     tile.innerHTML = emojis[num];
+    tile.onclick = function () {
+      if (tile.classList != "matched") {
+        if (activeTile == null) {
+          console.log("clicked active tile");
+          activeTile = tile;
+          return;
+        }
+      }
+      if (activeTile.innerHTML != tile.innerHTML) {
+        console.log("clicked but not matched");
+        activeTile = null;
+      }
+      if (activeTile.innerHTML == tile.innerHTML) {
+        activeTile.classList.add("matched");
+        activeTile.classList.add("matched");
+        activeTile = null;
+        console.log("clicked and matched");
+        revealedAmount += 2;
+        if (revealedAmount == 16) {
+          return "Game Won!";
+        }
+        return;
+      }
+    };
     document.querySelector(".container").appendChild(tile);
     tempArray.splice(num, 1);
   }
